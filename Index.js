938a5e252b1e19c777d57ccb0bb19b38465c00d4
Ademir jose de Deus <ademirjosededeus188@gmail.com>
2023-12-06 19:33:34 -0200

    Classes de um jogo

---
+++ Index.js
@@ -0,0 +1,39 @@
+// Progeto Classes de um Jogo
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque = "";
+
+        switch (this.tipo) {
+            case "mago":
+                ataque = "usou magia";
+                break;
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+            default:
+                ataque = "atacou";
+                break;
+        }
+
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso da classe
+const heroi1 = new Heroi("Aragorn", 30, "guerreiro");
+const heroi2 = new Heroi("Gandalf", 200, "mago");
+
+heroi1.atacar();
+heroi2.atacar();
